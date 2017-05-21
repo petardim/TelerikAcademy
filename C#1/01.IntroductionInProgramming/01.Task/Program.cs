edaafdc525632aea7543a3be14e48108797a60b9
Unknown <petar.dimitrov.it@gmail.com>
2017-05-21 20:23:40 +0300

    test

--- C#1/01.IntroductionInProgramming/01.Task/Program.cs
+++ C#1/01.IntroductionInProgramming/01.Task/Program.cs
@@ -6,7 +6,7 @@ namespace _01.Task
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Helffssrld!");
         }
     }
 }
